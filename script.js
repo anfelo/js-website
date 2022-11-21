b2e9af85557adefdacb823e603a39378aa596b91
francwow <francwow06@gmail.com>
2022-11-21 18:46:16 -0500

    Add user modal

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const jsWebsite = () => {
   // create and select elements 
     
     // html 
-    const html = document.getElementsByTagName('html')[0];
+    const htmlDoc = document.getElementsByTagName('html')[0];
 
     // head
     const head = document.head;
@@ -19,10 +19,17 @@ const jsWebsite = () => {
       const header = document.createElement('header');
       const logoContainer = document.createElement('div');
       const logo = document.createElement('img');
-      const h1 = document.createElement('h1');
+      const headerRight = document.createElement('div');
       const searchContainer = document.createElement('div');
       const search = document.createElement('input');
       const searchIcon = document.createElement('span');
+      const userContainer = document.createElement('div');
+      const userIcon = document.createElement('i');
+      const userMenuContainer = document.createElement('div');
+      const userMenuArrow = document.createElement('div');
+      const userMenuOptions = document.createElement('div');
+      const userLogin = document.createElement('p');
+
 
       // main
       const main = document.createElement('main');
@@ -32,6 +39,14 @@ const jsWebsite = () => {
       const ofertas = document.createElement('div');
       const footer = document.createElement('footer');
       const categoriesContainer = document.createElement('div'); 
+      const icons = document.createElement('div');
+      const icon1 = document.createElement('i');
+      const icon2 = document.createElement('i');
+      const icon3 = document.createElement('i');
+      const icon4 = document.createElement('i');
+      const icon5 = document.createElement('i');
+      const icon6 = document.createElement('i');
+      const icon7 = document.createElement('i');
       const categories = document.createElement('ul');
       const categoriesItem1 = document.createElement('li');
       const categoriesItem2 = document.createElement('li');
@@ -40,14 +55,6 @@ const jsWebsite = () => {
       const categoriesItem5 = document.createElement('li');
       const categoriesItem6 = document.createElement('li');
       const categoriesItem7 = document.createElement('li');
-
-
-
-  // testing stuff with console.log
-  const consoleLogs = () => {
-    console.log(html)
-  }
-  consoleLogs();
 
 
   // append elements
@@ -64,10 +71,17 @@ const jsWebsite = () => {
 
     // header
     header.append(logoContainer);
-    header.append(searchContainer);
+    header.append(headerRight);
+    header.append(userMenuContainer);
+    headerRight.append(searchContainer);
+    headerRight.append(userContainer);
     searchContainer.append(search);
     logoContainer.append(logo);
     searchContainer.append(searchIcon);
+    userContainer.append(userIcon);
+    userMenuContainer.append(userMenuArrow);
+    userMenuContainer.append(userMenuOptions);
+    userMenuOptions.append(userLogin);
 
     // main
     main.append(mainContent);
@@ -76,8 +90,11 @@ const jsWebsite = () => {
     mainContent.append(ofertas);
     mainContent.append(footer);
     sideMenu.append(categoriesContainer);
+    categoriesContainer.append(icons);
+    icons.append(icon1, icon2, icon3, icon4, icon5, icon6, icon7);
     categoriesContainer.append(categories);
     categories.append(categoriesItem1, categoriesItem2, categoriesItem3, categoriesItem4, categoriesItem5, categoriesItem6, categoriesItem7);
+    
 
   }
   appendElements();
@@ -91,7 +108,7 @@ const jsWebsite = () => {
 
     // header
     searchIcon.innerHTML = 'search';
-    h1.innerHTML = 'Welcome to our market!';
+    userLogin.innerHTML = 'Login';
     
     // sideMenu list elements 
     categoriesItem1.innerHTML = 'Vegetables';
@@ -110,8 +127,8 @@ const jsWebsite = () => {
   const styles = () => {
 
     // html 
-    html.style.fontFamily = 'Open Sans, sans-serif';
-    html.style.boxSizing = 'border-box';
+    htmlDoc.style.fontFamily = 'Open Sans, sans-serif';
+    htmlDoc.style.boxSizing = 'border-box';
 
     // head
     searchLink.rel = 'stylesheet';
@@ -123,6 +140,7 @@ const jsWebsite = () => {
     body.style.margin = 0;
 
     // header
+    header.style.position = 'relative';
     header.style.display = 'flex';
     header.style.justifyContent = 'space-between';
     header.style.alignItems = 'center';
@@ -130,15 +148,17 @@ const jsWebsite = () => {
     header.style.margin = '0 auto 50px';
 
       // h1
-      h1.style.textAlign = 'center';
-      h1.style.color = '#ff6600';
-      h1.style.textDecoration = 'underline';
 
       // logo
       logoContainer.style.width = '200px';
       logo.src = 'images/eat_well.png'
       logo.style.maxWidth = '100%';
       logo.style.height = 'auto';
+
+      // headerRight
+      headerRight.style.display = 'flex';
+      headerRight.style.justifyContent = 'space-between';
+      headerRight.style.alignItems = 'center';
 
       // search
       searchContainer.style.display = 'flex';
@@ -164,10 +184,47 @@ const jsWebsite = () => {
       searchIcon.style.justifyContent = 'center';
       searchIcon.style.alignItems = 'center';
 
+      // user
+      userContainer.style.marginLeft = '50px';
+      userContainer.style.borderRadius = '5px';
+      userContainer.style.position = 'relative';
+      userContainer.style.width = '70px';
+      userContainer.style.height = '70px';
+      userContainer.style.display = 'flex';
+      userContainer.style.justifyContent = 'center';
+      userContainer.style.alignItems = 'center';
+      userContainer.style.cursor = 'pointer';
+      userIcon.style.color = 'cadetblue';
+      userMenuContainer.style.position = 'absolute';
+      userMenuContainer.style.backgroundColor = 'gray';
+      userMenuContainer.style.width = '250px';
+      userMenuContainer.style.top = '120px';
+      userMenuContainer.style.right = '20px';
+      userMenuArrow.style.backgroundColor = 'gray';
+      userMenuArrow.style.position = 'absolute';
+      userMenuArrow.style.right = '10px';
+      userMenuArrow.style.top = '-5px';
+      userMenuArrow.style.width = '15px';
+      userMenuArrow.style.height = '15px';
+      userMenuArrow.style.transform = 'rotate(-45deg)'
+
+        // hover interaction
+        userContainer.addEventListener('mouseover', () => {
+          userContainer.style.backgroundColor = 'whitesmoke';
+          userContainer.style.transition = 'background-color .2s ease-in-out';
+        });
+
+        userContainer.addEventListener('mouseout', () => {
+          userContainer.style.backgroundColor = 'initial';
+          userContainer.style.transition = 'background-color .2s ease-in-out';
+        });
+
     // main
 
       // mainContent
       mainContent.style.display = 'grid';
+      mainContent.style.gridTemplateColumns = '200px repeat(2, 1fr)';
+      mainContent.style.gridTemplateRows = 'repeat(3, 1fr) 100px';
       mainContent.style.gridTemplateAreas = `'sideMenu mainMenu mainMenu' 'sideMenu mainMenu mainMenu' 'sideMenu ofertas ofertas' 'sideMenu footer footer'`;
       mainContent.style.width = '90%';
       mainContent.style.margin = 'auto';
@@ -185,40 +242,90 @@ const jsWebsite = () => {
       ofertas.style.marginBottom = '10px';
       footer.style.gridArea = 'footer';
       footer.style.backgroundColor = 'seaGreen';
+      icons.style.height = '100%';
+      icons.style.display = 'flex';
+      icons.style.flexDirection = 'column';
+      icons.style.justifyContent = 'center';
+      icons.style.alignItems = 'center';
       categoriesContainer.style.height = '100%';
       categoriesContainer.style.display = 'flex';
-      categoriesContainer.style.flexDirection = 'column';
+      categoriesContainer.style.flexDirection = 'row';
       categoriesContainer.style.justifyContent = 'center';
       categoriesContainer.style.alignItems = 'center';
+      icons.style.padding = '0';
+      icons.style.display = 'flex';
+      icons.style.flexDirection = 'column';
+      icons.style.justifyContent = 'space-around';
+      icons.style.height = '80%';
+      icons.style.margin = '0 10px';
+      icon1.style.color = 'seaGreen';
+      icon2.style.color = 'seaGreen';
+      icon3.style.color = 'seaGreen';
+      icon4.style.color = 'seaGreen';
+      icon5.style.color = 'seaGreen';
+      icon6.style.color = 'seaGreen';
+      icon7.style.color = 'seaGreen';
       categories.style.padding = '0';
       categories.style.display = 'flex';
       categories.style.flexDirection = 'column';
       categories.style.justifyContent = 'space-around';
       categories.style.height = '80%';
 
-      // style the list elements in side Menu
-      let liArray = document.querySelectorAll('li');
+        // style the list elements in side Menu
+        let liArray = document.querySelectorAll('li');
 
-
-      liArray.forEach(li => {
-        li.style.listStyle = 'none';
-        li.style.color = 'seaGreen';
-        li.style.fontWeight = '500';
-        li.style.cursor = 'pointer';
-
-        li.addEventListener('mouseover', () => {
-          li.style.color = 'darkGreen';
-          li.style.textDecoration = 'underline';
-          li.style.transition = 'color .3s ease-in-out';
-        });
-
-        li.addEventListener('mouseout', () => {
+        liArray.forEach(li => {
+          li.style.listStyle = 'none';
           li.style.color = 'seaGreen';
-          li.style.textDecoration = 'none';
+          li.style.fontWeight = '500';
+          li.style.cursor = 'pointer';
+
+          // hover interaction
+          li.addEventListener('mouseover', () => {
+            li.style.color = 'darkGreen';
+            li.style.textDecoration = 'underline';
+            li.style.transition = 'color .3s ease-in-out';
+          });
+
+          li.addEventListener('mouseout', () => {
+            li.style.color = 'seaGreen';
+            li.style.textDecoration = 'none';
+          });
         });
-      });
+
+      
+      // add classes
+      const addClasses = () => {
+
+        // create classes (preguntar a pipe). CONTINUAR AQUI
+        const isRed = `background-color: red;`;
+        userMenuArrow.style.cssText += isRed;
+        
+        // user
+        userIcon.classList.add('fa-solid', 'fa-user', 'fa-3x');
+
+        // icons
+        icon1.classList.add('fa-solid', 'fa-carrot', 'fa-lg');
+        icon2.classList.add('fa-solid', 'fa-apple-whole', 'fa-lg');
+        icon3.classList.add('fa-solid', 'fa-bread-slice', 'fa-lg');
+        icon4.classList.add('fa-solid', 'fa-bacon', 'fa-lg');
+        icon5.classList.add('fa-solid', 'fa-pepper-hot', 'fa-lg');
+        icon6.classList.add('fa-solid', 'fa-cheese', 'fa-lg');
+        icon7.classList.add('fa-solid', 'fa-wine-bottle', 'fa-lg');
+
+      }
+      addClasses();
+
   }
   styles();
+
+  
+  // testing stuff with console.log
+  const consoleLogs = () => {
+    console.log(body)
+    console.log(userMenuArrow.style.cssText)
+  }
+  consoleLogs();
 
 }
 

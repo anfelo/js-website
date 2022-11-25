4375263cbd6f04bf0d6cbc6a935026b837213395
francwow <francwow06@gmail.com>
2022-11-25 18:29:29 -0500

    add footer and ofertas content

--- script.js
+++ script.js
@@ -41,10 +41,9 @@ const jsWebsite = () => {
   // main
   const main = document.createElement("main");
   const mainContent = document.createElement("div");
+
+  // sideMenu
   const sideMenu = document.createElement("div");
-  const mainMenu = document.createElement("div");
-  const ofertas = document.createElement("div");
-  const footer = document.createElement("footer");
   const categoriesContainer = document.createElement("div");
   const icons = document.createElement("div");
   const icon1 = document.createElement("i");
@@ -62,6 +61,47 @@ const jsWebsite = () => {
   const categoriesItem5 = document.createElement("li");
   const categoriesItem6 = document.createElement("li");
   const categoriesItem7 = document.createElement("li");
+
+  // mainMenu
+  const mainMenu = document.createElement("div");
+
+  // ofertas
+  const ofertas = document.createElement("div");
+  const ofertasInfoContainer1 = document.createElement("div");
+  const ofertasInfoContainer2 = document.createElement("div");
+  const imageContainer1 = document.createElement("div");
+  const imageContainer2 = document.createElement("div");
+  const imageWeek = document.createElement("img");
+  const imageYear = document.createElement("img");
+  const infoWrapper1 = document.createElement("div");
+  const infoWrapper2 = document.createElement("div");
+  const infoHeader1 = document.createElement("h2");
+  const infoHeader2 = document.createElement("h2");
+  const infoDescription1 = document.createElement("p");
+  const infoDescription2 = document.createElement("p");
+
+  // footer
+  const footer = document.createElement("footer");
+  const footerInfo1 = document.createElement("div");
+  const footerInfo2 = document.createElement("div");
+  const footerAddress = document.createElement("div");
+  const addressIconAnchor = document.createElement("a");
+  const addressIcon = document.createElement("i");
+  const addressTextContainer = document.createElement("div");
+  const addressText1 = document.createElement("p");
+  const addressText2 = document.createElement("p");
+  const footerNewsletter = document.createElement("div");
+  const newsletterInput = document.createElement("input");
+  const newsletterBtn = document.createElement("button");
+  const footerSocial = document.createElement("div");
+  const iconAnchor1 = document.createElement("a");
+  const iconAnchor2 = document.createElement("a");
+  const iconAnchor3 = document.createElement("a");
+  const socialIcon1 = document.createElement("i");
+  const socialIcon2 = document.createElement("i");
+  const socialIcon3 = document.createElement("i");
+  const footerCopyright = document.createElement("div");
+  const copyrightText = document.createElement("p");
 
   // html function
   const htmlElement = () => {
@@ -333,7 +373,7 @@ const jsWebsite = () => {
     // add styles
     mainContent.style.display = "grid";
     mainContent.style.gridTemplateColumns = "200px repeat(2, 1fr)";
-    mainContent.style.gridTemplateRows = "repeat(3, 1fr) 100px";
+    mainContent.style.gridTemplateRows = "repeat(3, 1fr) 150px";
     mainContent.style.gridTemplateAreas = `'sideMenu mainMenu mainMenu' 'sideMenu mainMenu mainMenu' 'sideMenu ofertas ofertas' 'sideMenu footer footer'`;
     mainContent.style.width = "90%";
     mainContent.style.margin = "auto";
@@ -410,9 +450,10 @@ const jsWebsite = () => {
     categoriesContainer.style.display = "flex";
     categoriesContainer.style.flexDirection = "row";
     categoriesContainer.style.justifyContent = "center";
-    categoriesContainer.style.alignItems = "center";
+    categoriesContainer.style.alignItems = "start";
     categories.style.padding = "0";
     categories.style.display = "flex";
+    categories.style.margin = "0";
     categories.style.flexDirection = "column";
     categories.style.justifyContent = "space-around";
     categories.style.height = "80%";
@@ -461,18 +502,225 @@ const jsWebsite = () => {
 
   // ofertas function
   const ofertasElement = () => {
+    // append elements
+    ofertas.append(ofertasInfoContainer1, ofertasInfoContainer2);
+    ofertasInfoContainer1.append(imageContainer1, infoWrapper1);
+    imageContainer1.append(imageWeek);
+    infoWrapper1.append(infoHeader1, infoDescription1);
+    ofertasInfoContainer2.append(imageContainer2, infoWrapper2);
+    imageContainer2.append(imageYear);
+    infoWrapper2.append(infoHeader2, infoDescription2);
+
+    // innerHTML
+    imageWeek.src = "images/thanksgiving.png";
+    imageYear.src = "images/christmas.png";
+    infoHeader1.innerHTML = "Recipe of the Week";
+    infoDescription1.innerHTML = "Our great Thanksgiving Turkey";
+    infoHeader2.innerHTML = "Recipe of the Year";
+    infoDescription2.innerHTML = "The perfect Christmas Dinner";
+
     // add styles
     ofertas.style.gridArea = "ofertas";
     ofertas.style.backgroundColor = "gray";
     ofertas.style.marginBottom = "10px";
+    ofertas.style.display = "flex";
+    ofertas.style.justifyContent = "center";
+    ofertas.style.alignItems = "center";
+    ofertas.style.overflow = "hidden";
+    ofertasInfoContainer1.style.display = "flex";
+    ofertasInfoContainer1.style.flexDirection = "column";
+    ofertasInfoContainer1.style.alignItems = "center";
+    ofertasInfoContainer1.style.justifyContent = "center";
+    ofertasInfoContainer1.style.width = "50%";
+    ofertasInfoContainer1.style.overflow = "hidden";
+    ofertasInfoContainer1.style.position = "relative";
+    ofertasInfoContainer1.style.cursor = "pointer";
+    imageContainer1.style.width = "100%";
+    imageContainer1.style.zIndex = "1";
+    infoWrapper1.style.display = "flex";
+    infoWrapper1.style.flexDirection = "column";
+    infoWrapper1.style.justifyContent = "center";
+    infoWrapper1.style.alignItems = "center";
+    infoWrapper1.style.position = "absolute";
+    infoWrapper1.style.top = "0";
+    infoWrapper1.style.right = "0";
+    infoWrapper1.style.left = "0";
+    infoWrapper1.style.bottom = "0";
+    infoWrapper1.style.zIndex = "99";
+    infoWrapper1.style.backgroundColor = "#00000069";
+    infoWrapper1.style.opacity = "0";
+    infoHeader1.style.color = "white";
+    infoHeader1.style.margin = "0";
+    infoDescription1.style.color = "white";
+    infoDescription1.style.margin = "0";
+    ofertasInfoContainer2.style.display = "flex";
+    ofertasInfoContainer2.style.flexDirection = "column";
+    ofertasInfoContainer2.style.alignItems = "center";
+    ofertasInfoContainer2.style.justifyContent = "center";
+    ofertasInfoContainer2.style.width = "50%";
+    ofertasInfoContainer2.style.overflow = "hidden";
+    ofertasInfoContainer2.style.position = "relative";
+    ofertasInfoContainer2.style.cursor = "pointer";
+    ofertasInfoContainer2.style.zIndex = "1";
+    imageContainer2.style.width = "100%";
+    imageContainer2.style.zIndex = "1";
+    infoWrapper2.style.display = "flex";
+    infoWrapper2.style.flexDirection = "column";
+    infoWrapper2.style.justifyContent = "center";
+    infoWrapper2.style.alignItems = "center";
+    infoWrapper2.style.position = "absolute";
+    infoWrapper2.style.top = "0";
+    infoWrapper2.style.right = "0";
+    infoWrapper2.style.left = "0";
+    infoWrapper2.style.bottom = "0";
+    infoWrapper2.style.zIndex = "99";
+    infoWrapper2.style.backgroundColor = "#00000069";
+    infoWrapper2.style.opacity = "0";
+    infoHeader2.style.color = "white";
+    infoHeader2.style.margin = "0";
+    infoDescription2.style.color = "white";
+    infoDescription2.style.margin = "0";
+
+    const foodImages = ofertas.querySelectorAll("img");
+    foodImages.forEach((img) => {
+      img.style.maxWidth = "100%";
+      img.style.height = "auto";
+    });
+    console.log(foodImages);
+
+    const eventListeners = () => {
+      // hover on ofertas elements
+      ofertasInfoContainer1.addEventListener("mouseover", () => {
+        imageWeek.style.transform = "scale(1.2)";
+        imageWeek.style.transition = "transform .3s ease-in-out";
+        infoWrapper1.style.opacity = "1";
+        infoWrapper1.style.transition = "opacity .3s ease-in-out";
+      });
+
+      ofertasInfoContainer1.addEventListener("mouseout", () => {
+        imageWeek.style.transform = "scale(1)";
+        infoWrapper1.style.opacity = "0";
+      });
+
+      ofertasInfoContainer2.addEventListener("mouseover", () => {
+        imageYear.style.transform = "scale(1.2)";
+        imageYear.style.transition = "transform .3s ease-in-out";
+        infoWrapper2.style.opacity = "1";
+        infoWrapper2.style.transition = "opacity .3s ease-in-out";
+      });
+
+      ofertasInfoContainer2.addEventListener("mouseout", () => {
+        imageYear.style.transform = "scale(1)";
+        infoWrapper2.style.opacity = "0";
+      });
+    };
+    eventListeners();
   };
   ofertasElement();
 
   // footer function
   const footerElement = () => {
+    // append elements
+    footer.append(footerInfo1);
+    footerInfo1.append(footerAddress);
+    footerAddress.append(addressIconAnchor, addressTextContainer);
+    addressTextContainer.append(addressText1, addressText2);
+    addressIconAnchor.append(addressIcon);
+    footerInfo1.append(footerNewsletter);
+    footerNewsletter.append(newsletterInput, newsletterBtn);
+    footer.append(footerInfo2);
+    footerInfo2.append(footerCopyright, footerSocial);
+    footerSocial.append(iconAnchor1, iconAnchor2, iconAnchor3);
+    iconAnchor1.append(socialIcon1);
+    iconAnchor2.append(socialIcon2);
+    iconAnchor3.append(socialIcon3);
+    footerCopyright.append(copyrightText);
+
+    // innerHTML
+    addressText1.innerHTML = "Huertas Oriental Market";
+    addressText2.innerHTML = "Cajica, Colombia";
+    newsletterBtn.innerHTML = "Sign Up";
+    copyrightText.innerHTML =
+      "&copy; 2022 Huertas Oriental Market. All rights reserved.";
+
     // add atyles
     footer.style.gridArea = "footer";
-    footer.style.backgroundColor = "seaGreen";
+    footer.style.padding = "10px";
+    footer.style.display = "flex";
+    footer.style.flexDirection = "column";
+    footer.style.justifyContent = "center";
+    footer.style.alignItems = "center";
+    footerInfo1.style.width = "90%";
+    footerInfo1.style.margin = "10px auto";
+    footerInfo1.style.display = "flex";
+    footerInfo1.style.justifyContent = "space-around";
+    footerAddress.style.display = "flex";
+    footerAddress.style.justifyContent = "center";
+    footerAddress.style.alignItems = "center";
+    addressIconAnchor.href = "https://goo.gl/maps/9jAxmgVTEHdfPa2P6";
+    addressIconAnchor.target = "blank";
+    addressIconAnchor.style.margin = "0 10px";
+    addressIcon.style.color = "darkorange";
+    addressText1.style.margin = "0";
+    addressText1.style.color = "darkgray";
+    addressText2.style.color = "darkgray";
+    addressText2.style.margin = "0";
+    footerNewsletter.style.display = "flex";
+    footerNewsletter.style.justifyContent = "center";
+    footerNewsletter.style.alignItems = "center";
+    newsletterInput.placeholder = "sign up to our newsletter";
+    newsletterInput.style.marginRight = "10px";
+    newsletterInput.style.outline = "none";
+    newsletterInput.style.padding = "5px";
+    newsletterInput.style.border = "2px solid green";
+    newsletterInput.style.borderRadius = "5px";
+    newsletterBtn.style.backgroundColor = "white";
+    newsletterBtn.style.color = "darkorange";
+    newsletterBtn.style.height = "29px";
+    newsletterBtn.style.border = "2px solid darkorange";
+    newsletterBtn.style.borderRight = "3px solid darkorange";
+    newsletterBtn.style.borderBottom = "3px solid darkorange";
+    newsletterBtn.style.cursor = "pointer";
+    footerInfo2.style.width = "90%";
+    footerInfo2.style.margin = "0 auto";
+    footerInfo2.style.display = "flex";
+    footerInfo2.style.justifyContent = "space-around";
+    footerSocial.style.display = "flex";
+    footerSocial.style.justifyContent = "center";
+    footerSocial.style.alignItems = "center";
+
+    let footerIcons = footerSocial.childNodes;
+    footerIcons.forEach((icon, index) => {
+      icon.href = "#";
+      icon.style.color = "brown";
+      icon.style.margin = "5px";
+    });
+
+    copyrightText.style.color = "darkgray";
+    copyrightText.style.fontSize = ".8rem";
+
+    // eventListeners
+    const eventListeners = () => {
+      // Sign Up btn
+      newsletterBtn.addEventListener("mousedown", () => {
+        newsletterBtn.style.transform = "translateX(1px) translateY(1px)";
+        newsletterBtn.style.borderRight = "2px solid darkorange";
+        newsletterBtn.style.borderBottom = "2px solid darkorange";
+      });
+
+      newsletterBtn.addEventListener("mouseup", () => {
+        newsletterBtn.style.transform = "translateX(-1px) translateY(-1px)";
+        newsletterBtn.style.borderRight = "3px solid darkorange";
+        newsletterBtn.style.borderBottom = "3px solid darkorange";
+      });
+    };
+    eventListeners();
+
+    // add classes
+    addressIcon.classList.add("fa-solid", "fa-location-dot", "fa-lg");
+    socialIcon1.classList.add("fa-brands", "fa-facebook", "fa-2x");
+    socialIcon2.classList.add("fa-brands", "fa-instagram", "fa-2x");
+    socialIcon3.classList.add("fa-brands", "fa-youtube", "fa-2x");
   };
   footerElement();
 
